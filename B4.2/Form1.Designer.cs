e2a8b704021279ffe8d99ae9ec9b051f31cf9fb2
Manh Tuan <buimanhtuan2k4@gmail.com>
2024-11-14 10:01:04 +0700

    commit

---
+++ B4.2/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace B4._2
+{
+    partial class frMangGoc
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbMangGoc = new System.Windows.Forms.Label();
+            this.txtMangGoc = new System.Windows.Forms.TextBox();
+            this.txtKetQua = new System.Windows.Forms.TextBox();
+            this.lbKetQua = new System.Windows.Forms.Label();
+            this.btnXuatNgauNhien = new System.Windows.Forms.Button();
+            this.btnTongCacSoLe = new System.Windows.Forms.Button();
+            this.btnDemCacSoLe = new System.Windows.Forms.Button();
+            this.btnSapXepTang = new System.Windows.Forms.Button();
+            this.btnTimPhanTuNhoNhat = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblImageLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbMangGoc
+            // 
+            this.lbMangGoc.AutoSize = true;
+            this.lbMangGoc.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbMangGoc.Location = new System.Drawing.Point(46, 54);
+            this.lbMangGoc.Name = "lbMangGoc";
+            this.lbMangGoc.Size = new System.Drawing.Size(78, 20);
+            this.lbMangGoc.TabIndex = 0;
+            this.lbMangGoc.Text = "Mảng gốc";
+            this.lbMangGoc.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtMangGoc
+            // 
+            this.txtMangGoc.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMangGoc.Location = new System.Drawing.Point(50, 87);
+            this.txtMangGoc.Multiline = true;
+            this.txtMangGoc.Name = "txtMangGoc";
+            this.txtMangGoc.ReadOnly = true;
+            this.txtMangGoc.Size = new System.Drawing.Size(624, 39);
+            this.txtMangGoc.TabIndex = 1;
+            // 
+            // txtKetQua
+            // 
+            this.txtKetQua.Font = new System.Drawing.Font("Segoe UI", 15F);
+            this.txtKetQua.Location = new System.Drawing.Point(50, 184);
+            this.txtKetQua.Multiline = true;
+            this.txtKetQua.Name = "txtKetQua";
+            this.txtKetQua.ReadOnly = true;
+            this.txtKetQua.Size = new System.Drawing.Size(624, 39);
+            this.txtKetQua.TabIndex = 3;
+            // 
+            // lbKetQua
+            // 
+            this.lbKetQua.AutoSize = true;
+            this.lbKetQua.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbKetQua.Location = new System.Drawing.Point(46, 151);
+            this.lbKetQua.Name = "lbKetQua";
+            this.lbKetQua.Size = new System.Drawing.Size(61, 20);
+            this.lbKetQua.TabIndex = 2;
+            this.lbKetQua.Text = "Kết quả";
+            this.lbKetQua.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnXuatNgauNhien
+            // 
+            this.btnXuatNgauNhien.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnXuatNgauNhien.Location = new System.Drawing.Point(50, 259);
+            this.btnXuatNgauNhien.Name = "btnXuatNgauNhien";
+            this.btnXuatNgauNhien.Size = new System.Drawing.Size(264, 40);
+            this.btnXuatNgauNhien.TabIndex = 4;
+            this.btnXuatNgauNhien.Text = "Xuất ngẫu nhiên";
+            this.btnXuatNgauNhien.UseVisualStyleBackColor = true;
+            this.btnXuatNgauNhien.Click += new System.EventHandler(this.btnXuatNgauNhien_Click);
+            // 
+            // btnTongCacSoLe
+            // 
+            this.btnTongCacSoLe.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.btnTongCacSoLe.Location = new System.Drawing.Point(370, 259);
+            this.btnTongCacSoLe.Name = "btnTongCacSoLe";
+            this.btnTongCacSoLe.Size = new System.Drawing.Size(264, 40);
+            this.btnTongCacSoLe.TabIndex = 5;
+            this.btnTongCacSoLe.Text = "Tổng các số lẻ";
+            this.btnTongCacSoLe.UseVisualStyleBackColor = true;
+            this.btnTongCacSoLe.Click += new System.EventHandler(this.btnTongCacSoLe_Click);
+            // 
+            // btnDemCacSoLe
+            // 
+            this.btnDemCacSoLe.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.btnDemCacSoLe.Location = new System.Drawing.Point(50, 326);
+            this.btnDemCacSoLe.Name = "btnDemCacSoLe";
+            this.btnDemCacSoLe.Size = new System.Drawing.Size(264, 40);
+            this.btnDemCacSoLe.TabIndex = 6;
+            this.btnDemCacSoLe.Text = "Đếm các số lẻ";
+            this.btnDemCacSoLe.UseVisualStyleBackColor = true;
+            this.btnDemCacSoLe.Click += new System.EventHandler(this.btnDemCacSoLe_Click);
+            // 
+            // btnSapXepTang
+            // 
+            this.btnSapXepTang.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.btnSapXepTang.Location = new System.Drawing.Point(370, 326);
+            this.btnSapXepTang.Name = "btnSapXepTang";
+            this.btnSapXepTang.Size = new System.Drawing.Size(264, 40);
+            this.btnSapXepTang.TabIndex = 7;
+            this.btnSapXepTang.Text = "Sắp xếp tăng";
+            this.btnSapXepTang.UseVisualStyleBackColor = true;
+            this.btnSapXepTang.Click += new System.EventHandler(this.btnSapXepTang_Click);
+            // 
+            // btnTimPhanTuNhoNhat
+            // 
+            this.btnTimPhanTuNhoNhat.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.btnTimPhanTuNhoNhat.Location = new System.Drawing.Point(50, 387);
+            this.btnTimPhanTuNhoNhat.Name = "btnTimPhanTuNhoNhat";
+            this.btnTimPhanTuNhoNhat.Size = new System.Drawing.Size(264, 40);
+            this.btnTimPhanTuNhoNhat.TabIndex = 8;
+            this.btnTimPhanTuNhoNhat.Text = "Tìm phần tử nhỏ nhất";
+            this.btnTimPhanTuNhoNhat.UseVisualStyleBackColor = true;
+            this.btnTimPhanTuNhoNhat.Click += new System.EventHandler(this.btnTimPhanTuNhoNhat_Click);
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.btnThoat.Location = new System.Drawing.Point(370, 387);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(264, 40);
+            this.btnThoat.TabIndex = 9;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Blue;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(192, 30);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Code By TuanCute";
+            // 
+            // lblImageLabel
+            // 
+            this.lblImageLabel.AutoSize = true;
+            this.lblImageLabel.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblImageLabel.Location = new System.Drawing.Point(590, 9);
+            this.lblImageLabel.Name = "lblImageLabel";
+            this.lblImageLabel.Size = new System.Drawing.Size(84, 30);
+            this.lblImageLabel.TabIndex = 11;
+            this.lblImageLabel.Text = "Donate";
+            this.lblImageLabel.Click += new System.EventHandler(this.lblImageLabel_Click);
+            // 
+            // frMangGoc
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(686, 450);
+            this.Controls.Add(this.lblImageLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.btnTimPhanTuNhoNhat);
+            this.Controls.Add(this.btnSapXepTang);
+            this.Controls.Add(this.btnDemCacSoLe);
+            this.Controls.Add(this.btnTongCacSoLe);
+            this.Controls.Add(this.btnXuatNgauNhien);
+            this.Controls.Add(this.txtKetQua);
+            this.Controls.Add(this.lbKetQua);
+            this.Controls.Add(this.txtMangGoc);
+            this.Controls.Add(this.lbMangGoc);
+            this.Name = "frMangGoc";
+            this.Text = "Dãy số";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbMangGoc;
+        private System.Windows.Forms.TextBox txtMangGoc;
+        private System.Windows.Forms.TextBox txtKetQua;
+        private System.Windows.Forms.Label lbKetQua;
+        private System.Windows.Forms.Button btnXuatNgauNhien;
+        private System.Windows.Forms.Button btnTongCacSoLe;
+        private System.Windows.Forms.Button btnDemCacSoLe;
+        private System.Windows.Forms.Button btnSapXepTang;
+        private System.Windows.Forms.Button btnTimPhanTuNhoNhat;
+        private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblImageLabel;
+    }
+}
+
---
+++ B4.2/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace B4._2
+{
+    public partial class frMangGoc : Form
+    {
+        private int[] array;
+        public frMangGoc()
+        {
+            InitializeComponent();
+        }
+
+        private void lblImageLabel_Click(object sender, EventArgs e)
+        {
+            string imagePath = System.IO.Path.Combine(Application.StartupPath, "Resources", "qr-ng.jpg");
+
+            if (System.IO.File.Exists(imagePath))
+            {
+                Form formImage = new Form();
+                formImage.Text = "Donate";
+                formImage.Size = new Size(500, 500);
+
+                PictureBox pictureBox = new PictureBox();
+                pictureBox.Dock = DockStyle.Fill;
+                pictureBox.SizeMode = PictureBoxSizeMode.Zoom;
+                pictureBox.Image = Image.FromFile(imagePath);
+
+                formImage.Controls.Add(pictureBox);
+                formImage.ShowDialog();
+            }
+            else
+            {
+                MessageBox.Show("Không tìm thấy hình ảnh tại đường dẫn: " + imagePath);
+            }
+        }
+
+        private void btnXuatNgauNhien_Click(object sender, EventArgs e)
+        {
+            txtMangGoc.Text = mangNgauNhien();
+        }
+
+        private void btnTongCacSoLe_Click(object sender, EventArgs e)
+        {
+            if (array != null)
+            {
+                txtKetQua.Text = tongSoLe(array).ToString();
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng tạo mảng ngẫu nhiên trước!");
+            }
+        }
+
+        private void btnDemCacSoLe_Click(object sender, EventArgs e)
+        {
+            if(array != null)
+            {
+                txtKetQua.Text = demSoLe(array).ToString();
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng tạo mảng ngẫu nhiên trước!");
+            }
+        }
+
+        private void btnSapXepTang_Click(object sender, EventArgs e)
+        {
+            sapXepTang();
+        }
+
+        private void btnTimPhanTuNhoNhat_Click(object sender, EventArgs e)
+        {
+            timPhanTuNhoNhat();
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private string mangNgauNhien()
+        {
+            int n = 10;
+            array = new int[n];
+            Random rand = new Random();
+
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = rand.Next(-100, 101);
+            }
+
+            return string.Join(",", array);
+        }
+
+        private int tongSoLe(int[] array)
+        {
+            int ketQua = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] % 2 != 0)
+                {
+                    ketQua += array[i];
+                }
+            }
+            return ketQua;
+        }
+
+        private int demSoLe(int[] array)
+        {
+            int dem = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] % 2 != 0)
+                {
+                    dem++;
+                }
+            }
+            return dem;
+        }
+
+        private void sapXepTang()
+        {
+            if(array != null)
+            {
+                Array.Sort(array);
+                txtKetQua.Text = string.Join(",", array);
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng tạo mảng ngẫu nhiên trước!");
+            }
+        }
+
+        private void timPhanTuNhoNhat()
+        {
+            if(array != null && array.Length > 0)
+            {
+                int min = array.Min();
+                txtKetQua.Text = min.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng tạo mảng ngẫu nhiên trước!");
+            }
+        }
+    }
+}
